86bf41fb62036b87bc121d624a823426b148acba
daniel <daniel@daniel-laptop>
2014-03-08 22:46:10 +0100

    Added Handler methods for the buttons "new game", "replay game" and "undo".

--- GreenMahjong/www/js/mahjong.js
+++ GreenMahjong/www/js/mahjong.js
@@ -14,22 +14,54 @@ if (cordovaUsed()) {
  */
 function onDeviceReady() {
     var ubuntuUI = new UbuntuUI();
-    
+
     //needed because of a bug with "toolbar"
     UI = ubuntuUI;
     ubuntuUI.init();
 
     // Set up the app by pushing the main view
     ubuntuUI.pagestack.push("mainPage");
-    var toolbar = ubuntuUI.toolbar("footer");
-    toolbar.touch(function(){
-        toolbar.toggle();
-    });
     
+    var toolbar = ubuntuUI.toolbar("footer");
+    toolbar.touch(function(e) {
+        var el = e.srcElement || e.target;
+        if (el.id && /footer/i.test(el.id)) {
+            toolbar.toggle();
+        }
+    });
+
+    ubuntuUI.button('newGameButton').click(function(e) {
+        e.stopImmediatePropagation();
+        startNewGame();
+    });
+    ubuntuUI.button('replayGameButton').click(function(e) {
+        e.stopImmediatePropagation();
+        replayGame();
+    });
+    ubuntuUI.button('undoButton').click(function(e) {
+        e.stopImmediatePropagation();
+        undo();
+    });
+
     // var backButton = document.querySelector("li a[data-role=\"back\"]");
 
 
 }
+
+function startNewGame() {
+    console.log("Hier kommt der Code, um eine neues Spiel zu starten");
+}
+
+
+function replayGame() {
+    console.log("Hier kommt der Code, um das Spiel neu zu spielen");
+}
+
+
+function undo() {
+    console.log("Hier kommt der Code für die undo funktion.");
+}
+
 
 
 function showAlert(message) {
