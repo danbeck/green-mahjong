6fe042caa574ec3c6147dc46e7e2c995c3e345ee
daniel <daniel@daniel-laptop>
2014-04-02 22:33:58 +0200

    Compute the viewport with javascript.

--- GreenMahjong/www/js/mahjong.js
+++ GreenMahjong/www/js/mahjong.js
@@ -185,6 +185,17 @@ function onDeviceReady() {
         }
     });
 
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        var ww = ($(window).width() < window.screen.width) ? $(window).width() : window.screen.width; //get proper width
+        var mw = 1180; // min width of site
+        var ratio = ww / mw; //calculate ratio
+        if (ww < mw) { //smaller than minimum size
+            $('#Viewport').attr('content', 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=yes, width=' + ww);
+        } else { //regular size
+            $('#Viewport').attr('content', 'initial-scale=1.0, maximum-scale=2, minimum-scale=1.0, user-scalable=yes, width=' + ww);
+        }
+    }
+    
     ubuntuUI.button('newGameButton').click(function(e) {
         e.stopImmediatePropagation();
         startNewGame();
