5c83c10ee39d5bd24fa195d2346a46ccc4087ed0
daniel <daniel@daniel-laptop>
2014-04-02 22:57:02 +0200

    removed javascript code to set the viewport

--- GreenMahjong/www/js/mahjong.js
+++ GreenMahjong/www/js/mahjong.js
@@ -185,16 +185,16 @@ function onDeviceReady() {
         }
     });
 
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-        var ww = ($(window).width() < window.screen.width) ? $(window).width() : window.screen.width; //get proper width
-        var mw = 1180; // min width of site
-        var ratio = ww / mw; //calculate ratio
-        if (ww < mw) { //smaller than minimum size
-            $('#Viewport').attr('content', 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=yes, width=' + ww);
-        } else { //regular size
-            $('#Viewport').attr('content', 'initial-scale=1.0, maximum-scale=2, minimum-scale=1.0, user-scalable=yes, width=' + ww);
-        }
-    }
+//    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+//        var ww = ($(window).width() < window.screen.width) ? $(window).width() : window.screen.width; //get proper width
+//        var mw = 1180; // min width of site
+//        var ratio = ww / mw; //calculate ratio
+//        if (ww < mw) { //smaller than minimum size
+//            $('#Viewport').attr('content', 'initial-scale=' + ratio + ', maximum-scale=' + ratio + ', minimum-scale=' + ratio + ', user-scalable=yes, width=' + ww);
+//        } else { //regular size
+//            $('#Viewport').attr('content', 'initial-scale=1.0, maximum-scale=2, minimum-scale=1.0, user-scalable=yes, width=' + ww);
+//        }
+//    }
     
     ubuntuUI.button('newGameButton').click(function(e) {
         e.stopImmediatePropagation();
