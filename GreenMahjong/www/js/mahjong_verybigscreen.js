bbb1450f28a46c6bda2ef952be768ae45e338248
Daniel <d.danielbeck@googlemail.com>
2014-05-19 21:46:11 +0200

    New "verybigresolution".

---
+++ GreenMahjong/www/js/mahjong_verybigscreen.js
@@ -0,0 +1,11 @@
+var matchingGame = {};
+
+matchingGame.borderWidthRight = 12;
+matchingGame.borderWidthBelow = 11;
+//matchingGame.cardWidth = 80;
+matchingGame.cardWidth = 114;
+
+//matchingGame.cardHeight = 99;
+matchingGame.cardHeight = 141;
+
+matchingGame.shiftValue = 4;
