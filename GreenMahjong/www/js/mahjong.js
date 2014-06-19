a2052d675358b8fccc56e84ea62f663a29d9adf4
Karin <Karin@Karin-PC.fritz.box>
2014-06-19 09:11:57 +0200

    added new card layout

--- GreenMahjong/www/js/mahjong.js
+++ GreenMahjong/www/js/mahjong.js
@@ -46,115 +46,250 @@ matchingGame.selectableCards = {};
 matchingGame.matchingCards = {};
 
 matchingGame.positionX = [
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    4, 5, 6, 7, 8, 9, 10, 11,
-    3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-    3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-    4, 5, 6, 7, 8, 9, 10, 11,
-    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+    1, 2, 3,                9, 10, 11,
+    1,    3, 4, 5, 6, 7, 8, 9,     11,
+    1, 2, 3, 4,          8, 9, 10, 11,
+             4,          8,
+    1, 2, 3, 4,          8, 9, 10, 11,
+    1,    3, 4, 5, 6, 7, 8, 9,     11,
+    1, 2, 3,                9, 10, 11,
     // 2. Schicht
-    5, 6, 7, 8, 9, 10,
-    5, 6, 7, 8, 9, 10,
-    5, 6, 7, 8, 9, 10,
-    5, 6, 7, 8, 9, 10,
-    5, 6, 7, 8, 9, 10,
-    5, 6, 7, 8, 9, 10,
+    1, 2, 3,                9, 10, 11,
+    1,    3, 4, 5, 6, 7, 8, 9,     11,
+    1, 2, 3, 4,          8, 9, 10, 11,
+             4,          8,
+    1, 2, 3, 4,          8, 9, 10, 11,
+    1,    3, 4, 5, 6, 7, 8, 9,     11,
+    1, 2, 3,                9, 10, 11,
     // 3. Schicht
-    6, 7, 8, 9,
-    6, 7, 8, 9,
-    6, 7, 8, 9,
-    6, 7, 8, 9,
+    1, 2, 3,                9, 10, 11,
+    1,    3, 4,    6,    8, 9,     11,
+    1, 2, 3,                9, 10, 11,
+             4,          8,
+    1, 2, 3,                9, 10, 11,
+    1,    3, 4,    6,    8, 9,     11,
+    1, 2, 3,                9, 10, 11,
     // 4. Schicht
-    7, 8,
-    7, 8,
-    // 5. Schicht
-    7.5
+    1,                             11,
+    
+          3,                9,
+    
+    1,                             11,
+    
+          3,                9
 ];
 
 matchingGame.positionY = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    3.5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3.5, 3.5,
-    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
-    6, 6, 6, 6, 6, 6, 6, 6,
-    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+    0, 0, 0,                          0, 0, 0,
+    1,    1, 1.5, 1.5, 1.5, 1.5, 1.5, 1,    1,
+    2, 2, 2, 2.5,                2.5, 2, 2, 2,
+             3.5,                3.5,
+    5, 5, 5, 4.5,                4.5, 5, 5, 5,
+    6,    6, 5.5, 5.5, 5.5, 5.5, 5.5, 6,    6,
+    7, 7, 7,                          7, 7, 7,
     // 2. Schicht
-    1, 1, 1, 1, 1, 1,
-    2, 2, 2, 2, 2, 2,
-    3, 3, 3, 3, 3, 3,
-    4, 4, 4, 4, 4, 4,
-    5, 5, 5, 5, 5, 5,
-    6, 6, 6, 6, 6, 6,
+    0, 0, 0,                          0, 0, 0,
+    1,    1, 1.5, 1.5, 1.5, 1.5, 1.5, 1,    1,
+    2, 2, 2, 2.5,                2.5, 2, 2, 2,
+             3.5,                3.5,
+    5, 5, 5, 4.5,                4.5, 5, 5, 5,
+    6,    6, 5.5, 5.5, 5.5, 5.5, 5.5, 6,    6,
+    7, 7, 7,                          7, 7, 7,
     // 3. Schicht
-    2, 2, 2, 2,
-    3, 3, 3, 3,
-    4, 4, 4, 4,
-    5, 5, 5, 5,
+    0, 0, 0,                          0, 0, 0,
+    1,    1, 1.5,      1.5,      1.5, 1,    1,
+    2, 2, 2,                          2, 2, 2,
+             3.5,                3.5,
+    5, 5, 5,                          5, 5, 5,
+    6,    6, 5.5,      5.5,      5.5, 6,    6,
+    7, 7, 7,                          7, 7, 7,
     // 4. Schicht
-    3, 3,
-    4, 4,
-    // 5. Schicht
-    3.5
+    0,                                      0,
+    
+          2,                          2,
+    
+    5,                                      5,
+    
+          7,                          7
 ];
 
 matchingGame.shift = [
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
-    1, 1, 1, 1, 1, 1,
+    0, 0, 0,                0, 0, 0,
+    0,    0, 0, 0, 0, 0, 0, 0,    0,
+    0, 0, 0, 0,          0, 0, 0, 0,
+             0,          0,
+    0, 0, 0, 0,          0, 0, 0, 0,
+    0,    0, 0, 0, 0, 0, 0, 0,    0,
+    0, 0, 0,                0, 0, 0,
+    // 2. Schicht
+    1, 1, 1,                1, 1, 1,
+    1,    1, 1, 1, 1, 1, 1, 1,    1,
+    1, 1, 1, 1,          1, 1, 1, 1,
+             1,          1,
+    1, 1, 1, 1,          1, 1, 1, 1,
+    1,    1, 1, 1, 1, 1, 1, 1,    1,
+    1, 1, 1,                1, 1, 1,
     // 3. Schicht
-    2, 2, 2, 2,
-    2, 2, 2, 2,
-    2, 2, 2, 2,
-    2, 2, 2, 2,
+    2, 2, 2,                2, 2, 2,
+    2,    2, 2,    2,    2, 2,    2,
+    2, 2, 2,                2, 2, 2,
+             2,          2,
+    2, 2, 2,                2, 2, 2,
+    2,    2, 2,    2,    2, 2,    2,
+    2, 2, 2,                2, 2, 2,
     // 4. Schicht
-    3, 3,
-    3, 3,
-    // 5. Schicht
-    4
+    3,                            3,
+    
+          3,                3,
+    
+    3,                            3,
+    
+          3,                3
 ];
 
 matchingGame.selectable = [
-    true, false, false, false, false, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, false, false, false, false, false, false,
-    false, false, false, false, false, false, false, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, true,
-    true, false, false, false, false, false, false, false, false, false, false, true,
-    true, false, false, false, false, true,
-    true, false, false, false, false, true,
-    true, false, false, false, false, true,
-    true, false, false, false, false, true,
-    true, false, false, false, false, true,
-    true, false, false, false, false, true,
+    false, false, false,                                    false, false, false,
+    false,        false, false, false, false, false, false, false,        false,
+    false, false, false, false,                      false, false, false, false,
+                         false,                      false,
+    false, false, false, false,                      false, false, false, false,
+    false,        false, false, false, false, false, false, false,        false,
+    false, false, false,                                    false, false, false,
+    // 2. Schicht
+    false, false, false,                                    false, false, false,
+    false,        false, false, false, false, false, false, false,        false,
+    false, false, false, true,                       true,  false, false, false,
+                         false,                      false,
+    false, false, false, true,                       true,  false, false, false,
+    false,        false, false, false, false, false, false, false,        false,
+    false, false, false,                                    false, false, false,
     // 3. Schicht
-    true, false, false, true,
-    true, false, false, true,
-    true, false, false, true,
-    true, false, false, true,
+    false, false, true,                                     true,  false, false,
+    true,         true,  true,         true,         true,  true,         true,
+    true,  false, false,                                    false, false, true,
+                         true,                       true,
+    false, false, false,                                    false, false, false,
+    true,         true,  true,         true,         true,  true,         true,
+    true,  false, false,                                    false, false, true,
     // 4. Schicht
-    false, false,
-    false, false,
-    // 5. Schicht
-    true
+    true,                                                                 true,
+    
+                  true,                                     true,
+    
+    true,                                                                 true,
+    
+                  true,                                     true
 ];
 
+//matchingGame.positionX = [
+//    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+//    4, 5, 6, 7, 8, 9, 10, 11,
+//    3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+//    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+//    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+//    3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+//    4, 5, 6, 7, 8, 9, 10, 11,
+//    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+//    // 2. Schicht
+//    5, 6, 7, 8, 9, 10,
+//    5, 6, 7, 8, 9, 10,
+//    5, 6, 7, 8, 9, 10,
+//    5, 6, 7, 8, 9, 10,
+//    5, 6, 7, 8, 9, 10,
+//    5, 6, 7, 8, 9, 10,
+//    // 3. Schicht
+//    6, 7, 8, 9,
+//    6, 7, 8, 9,
+//    6, 7, 8, 9,
+//    6, 7, 8, 9,
+//    // 4. Schicht
+//    7, 8,
+//    7, 8,
+//    // 5. Schicht
+//    7.5
+//];
+//
+//matchingGame.positionY = [
+//    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//    1, 1, 1, 1, 1, 1, 1, 1,
+//    2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+//    3.5, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+//    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3.5, 3.5,
+//    5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+//    6, 6, 6, 6, 6, 6, 6, 6,
+//    7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7,
+//    // 2. Schicht
+//    1, 1, 1, 1, 1, 1,
+//    2, 2, 2, 2, 2, 2,
+//    3, 3, 3, 3, 3, 3,
+//    4, 4, 4, 4, 4, 4,
+//    5, 5, 5, 5, 5, 5,
+//    6, 6, 6, 6, 6, 6,
+//    // 3. Schicht
+//    2, 2, 2, 2,
+//    3, 3, 3, 3,
+//    4, 4, 4, 4,
+//    5, 5, 5, 5,
+//    // 4. Schicht
+//    3, 3,
+//    4, 4,
+//    // 5. Schicht
+//    3.5
+//];
+//
+//matchingGame.shift = [
+//    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//    0, 0, 0, 0, 0, 0, 0, 0,
+//    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//    0, 0, 0, 0, 0, 0, 0, 0,
+//    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+//    1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1,
+//    1, 1, 1, 1, 1, 1,
+//    // 3. Schicht
+//    2, 2, 2, 2,
+//    2, 2, 2, 2,
+//    2, 2, 2, 2,
+//    2, 2, 2, 2,
+//    // 4. Schicht
+//    3, 3,
+//    3, 3,
+//    // 5. Schicht
+//    4
+//];
+//
+//matchingGame.selectable = [
+//    true, false, false, false, false, false, false, false, false, false, false, true,
+//    true, false, false, false, false, false, false, true,
+//    true, false, false, false, false, false, false, false, false, true,
+//    true, false, false, false, false, false, false, false, false, false, false, false, false,
+//    false, false, false, false, false, false, false, false, false, false, false, false, false, true,
+//    true, false, false, false, false, false, false, false, false, true,
+//    true, false, false, false, false, false, false, true,
+//    true, false, false, false, false, false, false, false, false, false, false, true,
+//    true, false, false, false, false, true,
+//    true, false, false, false, false, true,
+//    true, false, false, false, false, true,
+//    true, false, false, false, false, true,
+//    true, false, false, false, false, true,
+//    true, false, false, false, false, true,
+//    // 3. Schicht
+//    true, false, false, true,
+//    true, false, false, true,
+//    true, false, false, true,
+//    true, false, false, true,
+//    // 4. Schicht
+//    false, false,
+//    false, false,
+//    // 5. Schicht
+//    true
+//];
 //matchingGame.cardWidthWithoutBorder = matchingGame.cardWidth - matchingGame.resolution.borderWidthRight;
 //matchingGame.cardHeightWithoutBorder = matchingGame.cardHeight - matchingGame.resolution.borderWidthBelow;
 
